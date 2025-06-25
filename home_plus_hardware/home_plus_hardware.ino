d96f9596ba540c2f150b290b587f6d0c7617bf1f
Erica Jiang <ericaijiang@gmail.com>
2025-06-25 18:32:23 -0400

    working relative position drive

--- home_plus_hardware/home_plus_hardware.ino
+++ home_plus_hardware/home_plus_hardware.ino
@@ -1,106 +1,78 @@
 #include <EEPROM.h> 
 #include <Servo.h>
-// #include <Encoder.h>
+#include <Encoder.h>
+#include <Math.h>
 
-float x, y, theta;
+float vx, vy, omega;
+int32_t target_x, target_y, target_theta;
+int32_t x, y, theta;
+float dx, dy, dtheta;
+float threshold = 100;
+
+float kP = 0.002;
+
+float wheel_radius = 0.096; // 96 mm
+float base_radius = 0.5 * (0.264+0.072*2) * sqrt(2); // square base with sides 264mm
+float wheel_circumference = PI * 0.096; // wheels are 96mm diameter
+float resolution = 2786.2;
 
 // === Servo Setup ===
-Servo servo1, servo2, servo3, servo4, servo5;
-int servo1Angle, servo2Angle, servo3Angle, servo4Angle, servo5Angle;
-
-Servo wrist, elbow, shoulder;
-int wristAngle, elbowAngle, shoulderAngle;
-
-// === Timing for Servos ===
-unsigned long previousServoMillis = 0;
-const long servoInterval = 10; // 10ms update interval
+Servo hand, wrist, elbow, shoulder;
+int hand_pos, wrist_pos, elbow_pos, shoulder_pos;
 
 // === Motor Pins ===
-
-// back left wheel
-#define SW_PWM 4 
-#define SW_CW 53
-#define SW_CCW 52
-// Encoder enone(22, 23);
-
-// front right wheel
-#define NE_PWM 5 
-#define NE_CCW 51
-#define NE_CW 50
-// Encoder entwo(24, 25);
-
-// front left wheel
 #define NW_PWM 6 
 #define NW_CW 49
 #define NW_CCW 48
-// Encoder enthree(26, 27);
+Encoder NW_ENCODER(21, 23);
 
-// back right wheel
+#define NE_PWM 5 
+#define NE_CW 50
+#define NE_CCW 51
+Encoder NE_ENCODER(20, 22);
+
+#define SW_PWM 4 
+#define SW_CW 53
+#define SW_CCW 52
+Encoder SW_ENCODER(2, 15);
+
 #define SE_PWM 7
 #define SE_CW 46
 #define SE_CCW 47
-// Encoder enfour(28, 29);
+Encoder SE_ENCODER(3, 14);
 
-// === Linear Actuators ===
-#define DIR1_LA 44
-#define DIR2_LA 45
-#define ENA1 41
+#define LA_DOWN 44
+#define LA_UP 45
+#define LA_PWM 41
 
-#define DIR1_LA2 42
-#define DIR2_LA2 43
-#define ENA2 40
-
-void stopAll();  // forward declaration
+#define FRAME_UP 42
+#define FRAME_DOWN 43
+#define FRAME_PWM 40
 
 void setup() {
     Serial.begin(9600);
 
-    /*
-    servo1.attach(10);
-    servo2.attach(11);
-    servo3.attach(12);
-    servo4.attach(13);
-    servo5.attach(9);
+    hand.attach(13);  
+    wrist.attach(12);
+    elbow.attach(11);
+    shoulder.attach(10);
 
-    servo1Angle = EEPROM.read(0);
-    servo2Angle = EEPROM.read(1);
-    servo3Angle = EEPROM.read(2);
-    servo4Angle = EEPROM.read(3);
-    servo5Angle = EEPROM.read(4);
+    hand_pos = 1000;
+    wrist_pos = 900;
+    elbow_pos = 1600;
+    shoulder_pos = 2100;
 
-    if (servo1Angle < 0 || servo1Angle > 180) servo1Angle = 90;
-    if (servo2Angle < 0 || servo2Angle > 180) servo2Angle = 90;
-    if (servo3Angle < 0 || servo3Angle > 180) servo3Angle = 90;
-    if (servo4Angle < 0 || servo4Angle > 180) servo4Angle = 90;
-    if (servo5Angle < 0 || servo5Angle > 180) servo5Angle = 90;
-
-    servo1.write(servo1Angle);
-    servo2.write(servo2Angle);
-    servo3.write(servo3Angle);
-    servo4.write(servo4Angle);
-    servo5.write(servo5Angle);
-    */
-
-    wrist.attach(13);
-    elbow.attach(12);
+    hand.write(hand_pos);
+    wrist.write(wrist_pos);
+    elbow.write(elbow_pos);
+    shoulder.write(shoulder_pos);
 
     Serial.println("USB Serial Ready");
     // printServoAngles();
 }
 
-
-void parseMessage(String input) {
-  int firstSpace = input.indexOf(' ');
-  int secondSpace = input.indexOf(' ', firstSpace + 1);
-
-  x = input.substring(0, firstSpace).toFloat();
-  y = input.substring(firstSpace + 1, secondSpace).toFloat();
-  theta = input.substring(secondSpace + 1).toFloat();
-}
-
 void loop() {
-    unsigned long currentMillis = millis();
-    static String lastCommand = "x";
+    static String lastCommand = " ";
 
     if (Serial.available() > 0) {
         lastCommand = "";
@@ -112,119 +84,87 @@ void loop() {
         }
     }
 
-    Serial.println(lastCommand);
+    read();
+    parseMessage(lastCommand);
 
+    dx = target_x - x;
+    dy = target_y - y;
+    dtheta = target_theta - theta;
+    
+    if (abs(dx) > threshold) { vx = kP*dx; } else { vx = 0; }
+    if (abs(dy) > threshold) { vy = kP*dy; } else { vy = 0; }
+    if (abs(dtheta) > threshold) { omega = kP*dtheta; } else { omega = 0; }
+
+    // reset when goal is reached
+    if (vx == 0  && vy == 0 && omega == 0) {
+        NW_ENCODER.write(0);
+        NE_ENCODER.write(0);
+        SW_ENCODER.write(0);
+        SE_ENCODER.write(0);
+
+        target_x = 0;
+        target_y = 0;
+        target_theta = 0;
+
+        lastCommand = " ";
+    }
+
+    drive(vx, vy, omega); 
+    Serial.println(String(target_x) + " " + String(target_y) + " " + String(target_theta));
+
+    /*
     if (lastCommand.length() == 1) {
         char cmd = lastCommand.charAt(0);
         switch (cmd) {
-            case '1':
-                writePercent(1, 0, 0);
-                delay(3000);
-
-                writePercent(0, 0, 0);
-                delay(500);
-
-                writePercent(0, 1, 0);
-                delay(5000);
-
-                writePercent(0, 0, 0);
-                break;
-            case '2':
-                writePercent(0, -1, 0);
-                delay(5000);
-
-                writePercent(0, 0, 0);
-                delay(500);
-                
-                writePercent(-1, 0, 0);
-                delay(3000);
-                
-                writePercent(0, 0, 0);
-                break;
-            case ' ': writePercent(0, 0, 0); break;
+            case ' ': drive(0, 0, 0); break;
         }
     } else {
         parseMessage(lastCommand);
-        writePercent(x, y, theta);
-        int time = 2000 * (abs(x)+abs(y)+abs(theta));
-        delay(time);
-        writePercent(0, 0, 0);
+        drive(vx, vy, omega);
+        // int time = 2000 * (abs(x)+abs(y)+abs(theta));
+        // delay(time);
+        // drive(0, 0, 0);
     }
-    
-    lastCommand = " ";
-
-    /* if (currentMillis - previousServoMillis >= servoInterval) {
-        previousServoMillis = currentMillis;
-        bool angleChanged = false;
-
-        for (int i = 0; i < lastCommand.length(); i++) {
-            char c = lastCommand.charAt(i);
-            switch (c) {
-                case '1': if (servo1Angle > 0) { servo1Angle--; servo1.write(servo1Angle); EEPROM.update(0, servo1Angle); angleChanged = true; } break;
-                case '2': if (servo1Angle < 180) { servo1Angle++; servo1.write(servo1Angle); EEPROM.update(0, servo1Angle); angleChanged = true; } break;
-                case '3': if (servo2Angle > 0) { servo2Angle--; servo2.write(servo2Angle); EEPROM.update(1, servo2Angle); angleChanged = true; } break;
-                case '4': if (servo2Angle < 180) { servo2Angle++; servo2.write(servo2Angle); EEPROM.update(1, servo2Angle); angleChanged = true; } break;
-                case '5': if (servo3Angle > 0) { servo3Angle--; servo3.write(servo3Angle); EEPROM.update(2, servo3Angle); angleChanged = true; } break;
-                case '6': if (servo3Angle < 180) { servo3Angle++; servo3.write(servo3Angle); EEPROM.update(2, servo3Angle); angleChanged = true; } break;
-                case '7': if (servo4Angle > 0) { servo4Angle--; servo4.write(servo4Angle); EEPROM.update(3, servo4Angle); angleChanged = true; } break;
-                case '8': if (servo4Angle < 180) { servo4Angle++; servo4.write(servo4Angle); EEPROM.update(3, servo4Angle); angleChanged = true; } break;
-                case '9': if (servo5Angle < 145) { servo5Angle++; servo5.write(servo5Angle); EEPROM.update(4, servo5Angle); angleChanged = true; } break;
-                case '0': if (servo5Angle > 80) { servo5Angle--; servo5.write(servo5Angle); EEPROM.update(4, servo5Angle); angleChanged = true; } break;
-            }
-        }
-
-        if (angleChanged) {
-            printServoAngles();
-        }
-    } */
+    // lastCommand = " ";
+    */
 }
 
-void executeSequence() {
-    // drive(0, 1);
-    Serial.println("Car moving forward...");
-    delay(2000);
-    // stopAll();
-    Serial.println("Car stopped.");
+void read() {
+    // positive counterclockwise
+    int32_t nw = NW_ENCODER.read();
+    int32_t ne = NE_ENCODER.read();
+    int32_t sw = SW_ENCODER.read();
+    int32_t se = SE_ENCODER.read();
 
-    controlLA(-1);
-    Serial.println("Linear Actuator Moving Down...");
-    delay(3000);
-    controlLA(0);
-    Serial.println("Linear Actuator Stopped.");
-
-    servo1Angle = constrain(servo1Angle + 10, 0, 180);
-    servo2Angle = constrain(servo2Angle + 10, 0, 180);
-    servo3Angle = constrain(servo3Angle + 10, 0, 180);
-    servo4Angle = constrain(servo4Angle + 10, 0, 180);
-    servo5Angle = constrain(servo5Angle + 10, 0, 180);
-
-    servo1.write(servo1Angle);
-    servo2.write(servo2Angle);
-    servo3.write(servo3Angle);
-    servo4.write(servo4Angle);
-    servo5.write(servo5Angle);
-
-    EEPROM.update(0, servo1Angle);
-    EEPROM.update(1, servo2Angle);
-    EEPROM.update(2, servo3Angle);
-    EEPROM.update(3, servo4Angle);
-    EEPROM.update(4, servo5Angle);
-
-    Serial.println("Servos moved +10 degrees.");
-    printServoAngles();
+    theta = (+ nw + ne + sw + se) / 4.0;
+    x = (- nw - ne + sw + se) / 4.0;
+    y = (+ nw - ne + sw - se) / 4.0;
 }
 
-void printServoAngles() {
-    Serial.print("Servo Angles -> ");
-    Serial.print("Servo1: "); Serial.print(servo1Angle);
-    Serial.print("°, Servo2: "); Serial.print(servo2Angle);
-    Serial.print("°, Servo3: "); Serial.print(servo3Angle);
-    Serial.print("°, Servo4: "); Serial.print(servo4Angle);
-    Serial.print("°, Servo5: "); Serial.print(servo5Angle);
-    Serial.println("°");
+void parseMessage(String input) {
+    int firstSpace = input.indexOf(' ');
+    int secondSpace = input.indexOf(' ', firstSpace + 1);
+
+    // velocity control
+    // vx = input.substring(0, firstSpace).toFloat();
+    // vy = input.substring(firstSpace + 1, secondSpace).toFloat();
+    // omega = input.substring(secondSpace + 1).toFloat();
+
+    float x_ = input.substring(0, firstSpace).toFloat();
+    float y_ = input.substring(firstSpace + 1, secondSpace).toFloat();
+    float theta_ = input.substring(secondSpace + 1).toFloat();
+
+    // convert meters / degrees to encoder pulses
+    target_x = (x_ / wheel_circumference) * resolution;
+    target_y = (y_ / wheel_circumference) * resolution;
+    target_theta = (((PI / 180) * base_radius * theta_) / wheel_circumference) * resolution;
 }
 
-void writePercent(float x, float y, float theta) {
+int32_t sgn(int32_t val) {
+    return val / abs(val);
+}
+
+void drive(float x, float y, float theta) {
     float nw = (x - y - theta);
     float ne = (x + y - theta);
     float sw = (x + y + theta);
@@ -260,32 +200,32 @@ void setMotorPower(int pwm, int cw, int ccw, float percent) {
 
 void controlLA(int direction) {
     if (direction > 0) {
-        digitalWrite(DIR1_LA, HIGH);
-        digitalWrite(DIR2_LA, LOW);
-        digitalWrite(ENA1, HIGH);
+        digitalWrite(LA_UP, HIGH);
+        digitalWrite(LA_DOWN, LOW);
+        digitalWrite(LA_PWM, HIGH);
     } else if (direction < 0) {
-        digitalWrite(DIR1_LA, LOW);
-        digitalWrite(DIR2_LA, HIGH);
-        digitalWrite(ENA1, HIGH);
+        digitalWrite(LA_UP, LOW);
+        digitalWrite(LA_DOWN, HIGH);
+        digitalWrite(LA_PWM, HIGH);
     } else {
-        digitalWrite(DIR1_LA, LOW);
-        digitalWrite(DIR2_LA, LOW);
-        digitalWrite(ENA1, LOW);
+        digitalWrite(LA_UP, LOW);
+        digitalWrite(LA_DOWN, LOW);
+        digitalWrite(LA_PWM, LOW);
     }
 }
 
-void controlLA2(int direction) {
+void controlFrame(int direction) {
     if (direction > 0) {
-        digitalWrite(DIR1_LA2, HIGH);
-        digitalWrite(DIR2_LA2, LOW);
-        digitalWrite(ENA2, HIGH);
+        digitalWrite(FRAME_UP, HIGH);
+        digitalWrite(FRAME_DOWN, LOW);
+        digitalWrite(FRAME_PWM, HIGH);
     } else if (direction < 0) {
-        digitalWrite(DIR1_LA2, LOW);
-        digitalWrite(DIR2_LA2, HIGH);
-        digitalWrite(ENA2, HIGH);
+        digitalWrite(FRAME_UP, LOW);
+        digitalWrite(FRAME_DOWN, HIGH);
+        digitalWrite(FRAME_PWM, HIGH);
     } else {
-        digitalWrite(DIR1_LA2, LOW);
-        digitalWrite(DIR2_LA2, LOW);
-        digitalWrite(ENA2, LOW);
+        digitalWrite(FRAME_UP, LOW);
+        digitalWrite(FRAME_DOWN, LOW);
+        digitalWrite(FRAME_PWM, LOW);
     }
 }
